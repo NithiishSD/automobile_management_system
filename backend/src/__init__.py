7f4564852882c714297da2a7408002977eafca53
Nithiish S.D <nithiishdanasekar@gmail.com>
2025-10-03 10:50:29 +0530

    changes in login py api

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask ,render_template,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 
+app=Flask(__name__)
 db=mysql.connector.connect(host='localhost',user='root',password='msc24pw24')
-
-
-
 if db.is_connected:
-    print("connected")
-
+    print("db connected")
 cur=db.cursor()
 cur.execute('use automobile')
-cur.execute("commit")
-app=Flask(__name__)
+
+
+from .login import loginbp
+
+app.register_blueprint(loginbp)
--- backend/src/login.py
+++ backend/src/login.py
@@ -1,17 +1,19 @@
-from flask import jsonify,request
-from src import app,cur
+from flask import jsonify,request,render_template,Blueprint
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from src import db
 
+loginbp=Blueprint('login',__name__)
 
-@app.route("/api/login",method=["POST","GET"])
-
+@loginbp.route('/login',methods=["POST","GET"])
 def login_user():
     userdetails=request.get_json()
     user=userdetails.get("user")
     password=userdetails.get("password")
-
-    cur.excute("select  * from users where username={%s} and password={%s}",(user,password))
     
-    y=cur.fetchall()
+    cur=db.cursor()
+    cur.execute("select  * from users where username=%s and password=%s",(user,password))
+    y=cur.fetchone()
+    print(y)
     cur.execute("commit")
     if y :
         return jsonify({'message':"user found"}),200
@@ -19,3 +21,12 @@ def login_user():
         return jsonify({'message':"user not found"}),401
     
     
+    
+@loginbp.route('/api/profile', methods=['GET'])
+@jwt_required()
+    
+def profile():
+    current_user_id = get_jwt_identity()
+    cur.excecute("select id,username from users where id={%s}",current_user_id)
+    user=cur.fetchone()
+    return jsonify({"id": user[0], "username": user[1]})
