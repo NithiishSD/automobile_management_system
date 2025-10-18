272008ea612ca4adb626e1b75931edbe62cad3e4
Nithiish S.D <nithiishdanasekar@gmail.com>
2025-10-18 12:36:11 +0530

    done service book get and post method endpoints

--- backend/src/servicebooking.py
+++ backend/src/servicebooking.py
@@ -1,10 +1,12 @@
 from flask import Blueprint,flask,request,jsonify
 from src import db
+from .utility import convert_tojson
 
 servicebookingbp=Blueprint('servicebookingbp',"__name__")
-cur=db.cursor()
+
 @servicebookingbp.route('/api/servicebookings', methods=['POST'])
 def create_service_booking():
+    cur=db.cursor()
     record= request.get_json()
 
     customer_id = record.get('customer_id')
@@ -27,3 +29,23 @@ def create_service_booking():
     cur.close()
 
     return jsonify({"message": "Service booking created successfully"}), 201
+
+
+@servicebookingbp.route('/api/servicebookings', methods=['GET'])
+def get_service_bookings():
+    cur = db.cursor()
+    cur.execute("""
+        SELECT sb.sid, c.name AS customer, v.model AS vehicle, st.name AS service_type,st.description as service_done, sb.appointment_date, sb.status,nvl(sr.completiondate,"service going on") 
+        FROM ServiceBooking sb
+        JOIN Customer c ON sb.customer_id = c.customer_id
+        JOIN Vehicle v ON sb.vehicle_id = v.vehicle_id
+        JOIN ServiceType st ON sb.sid = st.service_id
+        JOIN servicerecord sr on sb.sid=SR.servicebookid 
+        order by sb.appointment_date DESC""")
+    rows = cur.fetchall()
+    cur.close()
+    columns=["servicebookid","customer_name","model","servicetype","service_description","service_date","status"]
+    bookings=convert_tojson(rows,columns)
+   
+
+    return jsonify(bookings)
